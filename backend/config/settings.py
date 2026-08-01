619d853231bed69f5080a1d94120df454eb157b0
Abdullah <abdullah.abd6282@gmail.com>
2026-08-01 22:32:36 +0500

    Prepare for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['.railway.app', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.18.47']
 # Application definition
 
 INSTALLED_APPS = [
